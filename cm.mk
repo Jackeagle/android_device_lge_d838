010fd9a558337efb60f91050a607d180269b5fec
Jackeagle <jackeagle102@gmail.com>
2014-11-25 20:39:43 +0530

    Add More Things - Bootanimation Size - BUILD FINGERPRINT

--- cm.mk
+++ cm.mk
@@ -1,6 +1,10 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
 # Release name
 PRODUCT_RELEASE_NAME := d838
 
@@ -16,3 +20,5 @@ PRODUCT_NAME := cm_d838
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := d838
 PRODUCT_MANUFACTURER := lge
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/b1w_open_tw/b1w:4.4.2/KOT49I.D83810f/D83810f.1400726268:user/release-keys
